40b00895de2c159270bdf9d5cce164884bbfb3e4
Chugh <mchugh@teksystems.com>
2019-07-08 12:58:43 -0400

    dropbox

---
+++ src/javaBasics/DropBox.java
@@ -0,0 +1,15 @@
+package javaBasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DropBox {
+
+	
+		public static void main(String[] args) throws InterruptedException {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\mchugh\\Desktop\\JavaTraining\\chromedriver_win32\\chromedriver.exe");
+			WebDriver driver=new ChromeDriver();
+			driver.get("https://www.google.com");
+	}
+
+}
